b11f7370f3d0d917a2bc8f681eb7ddf1bd8a9ad3
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-11-28 22:24:17 -0300

    feat(info): created this entity main services, interactors and models to allow innteracting with the backend when working with this kind of data

---
+++ src/app/interactors/info/info.interactor.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, Optional } from '@angular/core';
+
+import { IInfo } from '../../models/proxies/info.interface';
+
+import { environment } from '../../../environments/environment';
+
+import { IInfoInteractor } from './info.interactor.interface';
+import { InfoMockup } from './info.mockup';
+
+/**
+ * Interactor that allows interacting with the backend.
+ */
+@Injectable({
+  providedIn: 'root',
+})
+export class InfoInteractor implements IInfoInteractor {
+  constructor(
+    @Optional()
+    private readonly infoMockup: InfoMockup,
+    private readonly httpClient: HttpClient,
+  ) {}
+
+  /**
+   * Method that returns the default entity.
+   *
+   * @returns an object that represents the default entity.
+   */
+  async getDefault() {
+    if (environment.mocked) {
+      return this.infoMockup.getDefault();
+    }
+    return this.httpClient
+      .get<IInfo>(`${environment.routes.info}/default`)
+      .toPromise();
+  }
+}
---
+++ src/app/services/info/info.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+import { InfoInteractor } from '../../interactors/info/info.interactor';
+
+/**
+ * Service that deals with all the data related with the `info` entity.
+ */
+@Injectable({
+  providedIn: 'root',
+})
+export class InfoService {
+  constructor(private readonly infoInteractor: InfoInteractor) {}
+
+  /**
+   * Method that returns the default entity.
+   *
+   * @returns an object that represents the default entity.
+   */
+  async getDefault() {
+    return this.infoInteractor.getDefault();
+  }
+}
