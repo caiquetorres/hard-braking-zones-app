1de3f4db1d3b102959992b287573a0750f17d5f8
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-11-28 22:42:10 -0300

    feat(network): created the "NetworkService" class

---
+++ src/app/services/network/network.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Network } from '@capacitor/network';
+import { BehaviorSubject } from 'rxjs';
+
+/**
+ * Service that deals with network connection.
+ */
+@Injectable({
+  providedIn: 'root',
+})
+export class NetworkService {
+  /**
+   * Property that defines an object that informs if the application is
+   * connected with the internet.
+   */
+  public readonly connected$ = new BehaviorSubject<boolean>(void 0);
+
+  constructor() {
+    Network.addListener('networkStatusChange', (status) => {
+      this.connected$.next(status.connected);
+    });
+  }
+}
