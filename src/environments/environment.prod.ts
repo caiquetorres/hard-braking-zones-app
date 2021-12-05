df317868a871b706e901eb0631ab817ba583bc88
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-12-05 00:12:08 -0300

    build: created the "routes.version" environment property

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,13 @@ export const environment = {
   production: true,
   baseUrl: 'https://hard-braking-zones-backend-dev.herokuapp.com/v1',
   mocked: false,
+  version: '1.0.0',
   keys: {
     settings: 'UC-SETTINGS',
   },
   routes: {
     info: '/info',
     feedback: '/feedback',
+    version: '/version',
   },
 };
