6c64f978059c019cbf69d34964aeb736dd41868a
caiquetorres <caiquetorres.bastos@hotmail.com>
2022-02-12 16:43:19 -0300

    feat(point.interface.ts): created this interface to handle all the point data

---
+++ src/app/models/interfaces/point.interface.ts
@@ -0,0 +1,11 @@
+export interface IPoint {
+  latitude: number;
+  longitude: number;
+  speed: number;
+  accuracy: number;
+  acclX: number;
+  acclY: number;
+  acclZ: number;
+  deviceId: string;
+  timestamp: number;
+}
